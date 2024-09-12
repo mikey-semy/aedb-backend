ccac3488e58fe0a1f9b38d2500b3a61ebcc7bf6c
Mike <34812500+mikey-semy@users.noreply.github.com>
2024-09-12 11:52:30 +0000

    static files from const

--- app/main.py
+++ app/main.py
@@ -1,14 +1,13 @@
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.staticfiles import StaticFiles
-from app.const import app_params, uvicorn_params, static_path
+from app.const import app_params, uvicorn_params, static_params
 from app.core.config import cors_params
 from app.routers import main
 
 app = FastAPI(**app_params)
 
-app.mount("/static", StaticFiles(directory=static_path), "static")
+app.mount(**static_params)
 
 app.include_router(main.router)
 
