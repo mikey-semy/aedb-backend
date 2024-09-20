9007dfe7b0d8c428282314e2baa017aef1ddd433
MikhailTo <toshkin.mikhail@yandex.ru>
2024-09-20 11:55:55 +0300

    Remove unnecessary import of "Any" from typing module in manuals.py router.

--- app/routers/manuals.py
+++ app/routers/manuals.py
@@ -1,4 +1,4 @@
-from typing import List, Any
+from typing import List
 from fastapi import APIRouter, Query, Depends
 from sqlalchemy.orm import Session
 from app.schemas.manuals import ManualSchema, GroupSchema, CategorySchema
